a0b8853ad6adaf4a220243bd2b569105fcb81dc8
AlbinoB <albinobraganza21@gmail.com>
2018-11-13 23:59:51 +0530

    accept//display//beta 0.1

---
+++ selection_sort.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+	void accept(int *,int);
+	void display(int *,int);
+	void selectiosort(int *,int);
+int main()
+{
+
+	int arrsize,c;
+	cout<<"\n Enter the number of elements:";
+    cin>>arrsize;
+    int arr[arrsize];
+	do
+	{
+		cout<<"\n\n\n enter your choice \n 1.Accept \n 2.Display \n 3.Selection sort. \n 0.exit.";
+		cin>>c;
+		switch(c)
+		{
+			case 1:{
+
+				accept(arr,arrsize);
+				break;
+				}
+			case 2:{
+				display(arr,arrsize);
+				break;
+				}
+			case 3:{
+				selectiosort(arr,arrsize);
+				break;
+				}
+
+			 default:{
+					cout<<"\n wrong input entered!!!";
+					break;
+				      }
+		}
+	    cout<<"\n___________________________________________________\n";
+	}while(c!=0 && c<4 && c>0);
+	return 0;
+}
+
+	void accept(int *arr,int arrsize)
+	{
+		for(int i=0;i<arrsize;i++)
+        {
+            cout<<"\n enter the element at position "<<i+1<<":";
+            cin>>arr[i];
+        }
+
+	}
+
+	void display(int *arr,int arrsize)
+		{
+			for(int i=0;i<arrsize;i++)
+			{
+				cout<<arr[i]<<" ";
+			}
+		}
+
+void selectiosort(int *arr,int arrsize){
+
+}
+
